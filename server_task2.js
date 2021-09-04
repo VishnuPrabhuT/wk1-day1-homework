14af8bd4941f6ff4d6ff51b0f55c047d0223e71e
Vishnu <vprabh@pdisoftware.com>
2021-09-04 13:48:40 -0500

    used fs module to send html res

--- server_task2.js
+++ server_task2.js
@@ -1,4 +1,7 @@
+var fs = require("fs");
+var path = require("path");
 var app = require("express")();
+
 
 app.use(function (err, req, res, next) {
     console.error(err.stack);
@@ -6,15 +9,15 @@ app.use(function (err, req, res, next) {
 });
 
 app.get("/", (req, res) => {
-    res.sendFile("/public/home.html", { root: __dirname });
+    sendResponse(req, res, "home");
 });
 
 app.get("/about", (req, res) => {
-    res.sendFile("/public/About.html", { root: __dirname });
+    sendResponse(req, res, "about");
 });
 
 app.get("/contact", (req, res) => {
-    res.sendFile("/public/Contact.html", { root: __dirname });
+    sendResponse(req, res, "contact");
 });
 
 app.use("*", (req, res) => {
@@ -24,3 +27,32 @@ app.use("*", (req, res) => {
 app.listen(5000);
 
 console.log("The NodeJS server on port 5000 is now running....");
+
+
+function sendResponse(req, res, page) {
+    var filePath = path.resolve(`./public/${page}.html`);
+    var fileExt = path.extname(filePath);
+
+    if (fileExt === '.html') {
+        fs.access(filePath, function (err) {
+            if (err) {
+                res.statusCode = 404;
+                res.setHeader('Content-Type', 'text/html');
+                res.end(`<html><body><h1>Error 404: ${fileUrl} not
+            found</h1></body></html>`);
+                return;
+            }
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+
+            fs.createReadStream(filePath).pipe(res);
+
+        });
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(`<html><body><h1>Error 404: ${fileUrl} is
+            not an HTML file</h1></body></html>`);
+    }
+}
